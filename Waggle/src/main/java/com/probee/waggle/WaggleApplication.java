5ce9c0125eeeffa26842cb18d7abefe00176766c
hoon105204 <hoon105204@gmail.com>
2022-06-30 10:38:13 +0900

    first commit

---
+++ Waggle/src/main/java/com/probee/waggle/WaggleApplication.java
@@ -0,0 +1,22 @@
+package com.probee.waggle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@Controller
+public class WaggleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaggleApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public static String goindex() {
+		return "index";
+	}
+
+
+}
