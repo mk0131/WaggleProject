ca507e62b59e2acde3ceb5b0c0f4b2e10ca39d57
YangSeongHui <shxoxo0731@gmail.com>
2022-07-24 23:00:56 +0900

    inquiry paging

--- Waggle/src/main/java/com/probee/waggle/model/service/InquiryService.java
+++ Waggle/src/main/java/com/probee/waggle/model/service/InquiryService.java
@@ -2,16 +2,18 @@ package com.probee.waggle.model.service;
 
 import java.util.List;
 
+import com.probee.waggle.model.dto.Criteria;
 import com.probee.waggle.model.dto.InquiryDto;
 import com.probee.waggle.model.dto.UsersDto;
 
 public interface InquiryService {
 
-	public List<InquiryDto> selectList(int user_Code);
+	public List<InquiryDto> selectList(int user_Code, Criteria cri);
 	public UsersDto selectuser(int user_Code);
 	public InquiryDto selectOne(int in_Code);
 	public int insert(InquiryDto dto);
 	public int update(InquiryDto dto);
 	public int delete(int in_Code);
+	public int inquiryListCnt();
 	
 }
