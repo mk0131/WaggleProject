d687e673ccb3d956aa1582399a361a19f11a7f4a
hoon105204 <hoon105204@gmail.com>
2022-08-11 17:06:15 +0900

    mapper

--- Waggle/src/main/java/com/probee/waggle/model/mapper/BoardMapper.java
+++ Waggle/src/main/java/com/probee/waggle/model/mapper/BoardMapper.java
@@ -23,18 +23,18 @@ public interface BoardMapper {
 	
 	// 전체 request 가져오기
 	@Select(" select req_No,req_Title,req_Link, req_WDate,date_format(req_EDate,'%Y-%m-%d') req_EDate,req_Phone,req_Detail,req_Point,req_Stat,req_UCode,req_FCode,req_HCode,fi_Nm,home_Addr,home_DAddr,home_Lat,home_Lng "
-			+ "from request r left outer join file f on r.req_FCode = f.fi_Code left outer join home h on r.req_HCode = h.home_Code "
+			+ "from Request r left outer join File f on r.req_FCode = f.fi_Code left outer join Home h on r.req_HCode = h.home_Code "
 			+ "order by req_No desc LIMIT #{pageStart}, #{perPageNum} ")
 	public List<RequestDto2> selectList(Criteria cri);
 	
 	// 글번호로 request 모든 정보 불러오기
 	@Select(" select req_No,req_Title,req_Link, req_WDate,date_format(req_EDate,'%Y-%m-%d') req_EDate,req_Phone,req_Detail,req_Point,req_Stat,req_UCode,req_FCode,req_HCode,fi_Nm,home_Addr,home_DAddr,home_Lat,home_Lng "
-			+ "from request r left outer join file f on r.req_FCode = f.fi_Code left outer join home h on r.req_HCode = h.home_Code "
+			+ "from Request r left outer join File f on r.req_FCode = f.fi_Code left outer join Home h on r.req_HCode = h.home_Code "
 			+ "where req_No=#{req_No} ")
 	public RequestDto2 selectRequest(int req_No);
 	
 	// 글번호로 요청글에 등록된 파일코드 업데이트 하기
-	@Update(" update request set req_FCode=#{req_FCode} where req_No=#{req_No} ")
+	@Update(" update Request set req_FCode=#{req_FCode} where req_No=#{req_No} ")
 	public int updateFCode(int req_No, int req_FCode);
 	
 	// 요청글 등록하기
@@ -45,7 +45,7 @@ public interface BoardMapper {
 	@Select(" select * from Result where res_No=#{req_No} and res_Stat in ('진행중','확인중','완료') ")
 	public ResultDto selectResult(int req_No);
 	
-	@Select(" select user_Code, user_Pro, user_Grade, user_Intro, user_Nm, fi_Nm from Users u inner join volunteer v on u.user_Code = v.vo_Ucode left outer join File f on u.user_Pro=f.fi_Code where v.vo_No = #{vo_No} and v.vo_Block = false")
+	@Select(" select user_Code, user_Pro, user_Grade, user_Intro, user_Nm, fi_Nm from Users u inner join Volunteer v on u.user_Code = v.vo_Ucode left outer join File f on u.user_Pro=f.fi_Code where v.vo_No = #{vo_No} and v.vo_Block = false")
 	public List<UsersDto2> FindVol(int vo_No);
 	
 	// 유저정보 가져오기
@@ -72,7 +72,7 @@ public interface BoardMapper {
 	@Update(" update Request set req_Stat = '취소' where req_No = #{req_No} ")
 	int Revoke(int req_No);
 	
-	@Delete(" delete from request where req_No = #{req_No} ")
+	@Delete(" delete from Request where req_No = #{req_No} ")
 	int Cancel(int req_No);
 	
 	@Update(" update Request set req_Stat = '완료' where req_No = #{req_No} ")
@@ -84,7 +84,7 @@ public interface BoardMapper {
 	@Update(" update Request set req_Title=#{req_Title}, req_Link=#{req_Link}, req_EDate=#{req_EDate}, req_Phone=#{req_Phone}, req_Detail=#{req_Detail}, req_Point=#{req_Point}, req_HCode=#{req_HCode} where req_No=#{req_No} ")
 	public int updateRequest(RequestDto2 dto);
 	
-	@Select(" select max(req_No) from request r ")
+	@Select(" select max(req_No) from Request r ")
 	public int selectLastRequestNo();
 	
 	// 꿀벌 평가 DB 삽입
@@ -96,7 +96,7 @@ public interface BoardMapper {
 	public int insertReRatingBee(UserRatingDto userRating_dto);
 	
 	// 포인트 사용여부 검사
-	@Select(" select * from points p where po_No = #{req_No} and po_UCode = #{user_Code} ")
+	@Select(" select * from Points p where po_No = #{req_No} and po_UCode = #{user_Code} ")
 	List<PointsDto> selectPoint(int req_No, int user_Code);
 	
 	// result 업데이트(확인중)
@@ -107,11 +107,11 @@ public interface BoardMapper {
 			+ "where res_Code=#{res_Code} ")
 	public int updateResult(ResultDto dto);
 
-	@Select(" select count(*) from request order by req_No desc ")
+	@Select(" select count(*) from Request order by req_No desc ")
 	public int boardListCnt();
 	
 	// 유저코드로 사용자 주소 가져오기
-	@Select(" SELECT * FROM USERADDRESS WHERE ua_UCode = #{ua_UCode} ")
+	@Select(" SELECT * FROM UserAddress WHERE ua_UCode = #{ua_UCode} ")
 	public UserAddressDto selectUserAddr(int ua_Ucode);
 	
 	// 요청글 완료 전환시 Result DB 시간 업데이트
